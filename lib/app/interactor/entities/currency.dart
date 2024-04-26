5c5e4d2113fb23a38ee20f26d636ad0cea623618
João Pedro <joaopedropereira3232@hotmail.com>
2024-04-26 11:27:37 -0300

    add constants

--- lib/app/interactor/entities/currency.dart
+++ lib/app/interactor/entities/currency.dart
@@ -2,12 +2,10 @@ class Currency {
   final String code;
   final String name;
   final String flagUrl;
-  final String symbol;
 
   Currency({
     required this.code,
     required this.name,
     required this.flagUrl,
-    required this.symbol,
   });
 }
