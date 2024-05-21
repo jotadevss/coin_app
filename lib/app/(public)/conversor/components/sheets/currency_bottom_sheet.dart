dacf25cd1592713e1a86c13c1802952b6ba79d56
João Pedro <joaopedropereira3232@hotmail.com>
2024-05-21 17:33:16 -0300

    add currency bottom sheet

---
+++ lib/app/(public)/conversor/components/sheets/currency_bottom_sheet.dart
@@ -0,0 +1,110 @@
+import 'package:coin_app/app/interactor/models/currency.dart';
+import 'package:coin_app/app/shared/theme.dart';
+import 'package:coin_app/app/size_config.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CurrencyBottomSheet extends StatelessWidget {
+  const CurrencyBottomSheet({
+    super.key,
+    required this.currencies,
+  });
+
+  final List<Currency> currencies;
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 4,
+            width: 50,
+            decoration: BoxDecoration(
+              color: Colors.grey.withOpacity(0.1),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 16),
+            child: Text(
+              "Moedas disponíveis",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          TextField(
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(16),
+              hintText: "Procurar moedas",
+              hintStyle: TextStyle(
+                fontSize: 16,
+                height: 0,
+                color: AppStyle.kGrayFontColor.withOpacity(0.3),
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: SvgPicture.asset(
+                  "lib/assets/icons/search.svg",
+                  width: 18,
+                ),
+              ),
+              filled: true,
+              fillColor: AppStyle.kGrayFontColor.withOpacity(0.05),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+          const SizedBox(height: 12),
+          Expanded(
+            child: ListView.builder(
+              itemCount: currencies.length,
+              itemBuilder: (context, index) {
+                final currency = currencies[index];
+
+                return ListTile(
+                  onTap: () {},
+                  leading: CircleAvatar(
+                    radius: 20,
+                    child: SvgPicture.asset(
+                      AppStyle.getCurrencyFlag(currencies[0].code),
+                      width: 40,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                  title: Text(
+                    currency.name,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  subtitle: Text(
+                    currency.code,
+                    style: TextStyle(
+                      color: AppStyle.kGrayFontColor.withOpacity(0.5),
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  trailing: const Icon(Icons.arrow_forward_ios_rounded, size: 14),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/app/(public)/conversor/conversor_page.dart
+++ lib/app/(public)/conversor/conversor_page.dart
@@ -1,13 +1,16 @@
-import 'package:coin_app/app/(public)/conversor/components/currency_in_component.dart';
-import 'package:coin_app/app/(public)/conversor/components/currency_out_component.dart';
+import 'package:asp/asp.dart';
 import 'package:coin_app/app/(public)/conversor/components/filled_button_component.dart';
 import 'package:coin_app/app/(public)/conversor/components/flip_currencies_component.dart';
 import 'package:coin_app/app/(public)/conversor/components/input_value_form_component.dart';
 import 'package:coin_app/app/(public)/conversor/components/main_app_bar_component.dart';
 import 'package:coin_app/app/(public)/conversor/components/quotation_value_component.dart';
+import 'package:coin_app/app/(public)/conversor/components/select_currency_component.dart';
+import 'package:coin_app/app/(public)/conversor/components/sheets/currency_bottom_sheet.dart';
 import 'package:coin_app/app/(public)/conversor/components/underline_text_button_component.dart';
+import 'package:coin_app/app/interactor/atoms/currency_atoms.dart';
 import 'package:coin_app/app/interactor/models/currency.dart';
 import 'package:coin_app/app/shared/theme.dart';
+import 'package:coin_app/app/size_config.dart';
 import 'package:flutter/material.dart';
 
 class ConversorPage extends StatefulWidget {
@@ -18,50 +21,216 @@ class ConversorPage extends StatefulWidget {
 }
 
 class _ConversorPageState extends State<ConversorPage> {
-  void showCurrencyBottomSheet(List<Currency> allCurrencies) {}
+  @override
+  void initState() {
+    super.initState();
+    fetchAllCurrenciesAction.call();
+  }
+
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void showCurrencyBottomSheet(List<Currency> allCurrencies) {
+    final actualContext = _scaffoldKey.currentContext!;
+    SizeConfig().init(actualContext);
+
+    showModalBottomSheet(
+      context: actualContext,
+      elevation: 0,
+      isScrollControlled: true,
+      constraints: BoxConstraints(
+        maxHeight: SizeConfig.screenHeight! / 1.2,
+        minWidth: SizeConfig.screenWidth!,
+      ),
+      builder: (context) => CurrencyBottomSheet(currencies: allCurrencies),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final currencyState$ = context.select(() => currencyState.value);
+
+    final Widget? body = currencyState$.when(
+      currencyIn: (state) {
+        return Column(
+          children: [
+            const MainAppBar(),
+            Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 0),
+              child: SelectCurrency(
+                label: "De",
+                title: state.currencyIn.name,
+                subtitle: state.currencyIn.code,
+                iconPath: AppStyle.getCurrencyFlag(state.currencyIn.code),
+                onTap: () => showCurrencyBottomSheet(state.allCurrencies),
+                onClear: () {},
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 18),
+              child: SelectCurrency(
+                label: "Para",
+                title: "Selecione uma moeda",
+                subtitle: "Moeda secundária",
+                iconPath: "lib/assets/icons/arrow-in.svg",
+                onTap: () => showCurrencyBottomSheet(state.availableCurrenciesOut),
+                onClear: null,
+              ),
+            ),
+          ],
+        );
+      },
+      currencyOut: (state) {
+        return Column(
+          children: [
+            const MainAppBar(),
+            Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 0),
+              child: SelectCurrency(
+                label: "De",
+                title: "Selecione uma moeda",
+                subtitle: "Moeda primária",
+                iconPath: "lib/assets/icons/arrow-in.svg",
+                onTap: () => showCurrencyBottomSheet(state.availableCurrenciesIn),
+                onClear: null,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 18),
+              child: SelectCurrency(
+                label: "Para",
+                title: state.currencyOut.name,
+                subtitle: state.currencyOut.code,
+                iconPath: AppStyle.getCurrencyFlag(state.currencyOut.code),
+                onTap: () => showCurrencyBottomSheet(state.allCurrencies),
+                onClear: () {},
+              ),
+            ),
+          ],
+        );
+      },
+      init: (state) {
+        return Column(
+          children: [
+            const MainAppBar(),
+            Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 0),
+              child: SelectCurrency(
+                label: "De",
+                title: "Selecione uma moeda",
+                subtitle: "Moeda primária",
+                iconPath: "lib/assets/icons/arrow-in.svg",
+                onTap: () => showCurrencyBottomSheet([]),
+                onClear: null,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 18),
+              child: SelectCurrency(
+                label: "Para",
+                title: "Selecione uma moeda",
+                subtitle: "Moeda secundária",
+                iconPath: "lib/assets/icons/arrow-in.svg",
+                onTap: () => showCurrencyBottomSheet([]),
+                onClear: null,
+              ),
+            ),
+          ],
+        );
+      },
+      success: (state) {
+        return Column(
+          children: [
+            const MainAppBar(),
+            Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 0),
+              child: SelectCurrency(
+                label: "De",
+                title: state.currencyIn.name,
+                subtitle: state.currencyIn.code,
+                iconPath: AppStyle.getCurrencyFlag(state.currencyIn.code),
+                onTap: () => showCurrencyBottomSheet(state.availableCurrenciesIn),
+                onClear: () {},
+              ),
+            ),
+            const InputValueForm(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 18),
+              child: FlipCurrencies(onPressed: () {}, enable: state.canFlipCurrencies),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 18),
+              child: SelectCurrency(
+                label: "Para",
+                title: state.currencyOut.name,
+                subtitle: state.currencyOut.code,
+                iconPath: AppStyle.getCurrencyFlag(state.currencyOut.code),
+                onTap: () => showCurrencyBottomSheet(state.availableCurrenciesOut),
+                onClear: () {},
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 28),
+              child: QuotationValue(),
+            ),
+            UnderlineTextButton(
+              title: "Limpar todas moedas selecionadas",
+              onPressed: () {},
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 12, bottom: 10),
+              child: CustomFilledButton(onPressed: () {}),
+            ),
+            const Text(
+              "Ultima atualização ás 13:54",
+              style: TextStyle(
+                color: AppStyle.kGrayFontColor,
+                fontWeight: FontWeight.w500,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 32),
+          ],
+        );
+      },
+      empty: (state) {
+        return Column(
+          children: [
+            const MainAppBar(),
+            Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 0),
+              child: SelectCurrency(
+                label: "De",
+                title: "Selecione uma moeda",
+                subtitle: "Moeda primária",
+                iconPath: "lib/assets/icons/arrow-in.svg",
+                onTap: () => showCurrencyBottomSheet(state.allCurrencies),
+                onClear: null,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 18),
+              child: SelectCurrency(
+                label: "Para",
+                title: "Selecione uma moeda",
+                subtitle: "Moeda secundária",
+                iconPath: "lib/assets/icons/arrow-in.svg",
+                onTap: () => showCurrencyBottomSheet(state.allCurrencies),
+                onClear: null,
+              ),
+            ),
+          ],
+        );
+      },
+      error: (state) => null,
+    );
+
     return Scaffold(
+      key: _scaffoldKey,
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                const MainAppBar(),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16, bottom: 0),
-                  child: CurrencyIn(showCurrencyBottomSheet: showCurrencyBottomSheet),
-                ),
-                const InputValueForm(),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 18),
-                  child: FlipCurrencies(onPressed: () {}, enable: true),
-                ),
-                CurrencyOut(showCurrencyBottomSheet: showCurrencyBottomSheet),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 28),
-                  child: QuotationValue(),
-                ),
-                UnderlineTextButton(
-                  title: "Limpar todas moedas selecionadas",
-                  onPressed: () {},
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 12, bottom: 10),
-                  child: CustomFilledButton(onPressed: () {}),
-                ),
-                const Text(
-                  "Ultima atualização ás 13:54",
-                  style: TextStyle(
-                    color: AppStyle.kGrayFontColor,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(height: 32),
-              ],
-            ),
+            child: body,
           ),
         ),
       ),
