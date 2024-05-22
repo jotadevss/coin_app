4ca5f21420fa404202b3153c3c29d93446b78131
João Pedro <joaopedropereira3232@hotmail.com>
2024-05-21 22:47:04 -0300

    solve dependecy injection

---
+++ lib/app/injector.dart
@@ -0,0 +1,32 @@
+import 'package:auto_injector/auto_injector.dart';
+import 'package:coin_app/app/external/repositories/api/http_quote_repository.dart';
+import 'package:coin_app/app/external/repositories/api/http_variation_repository.dart';
+import 'package:coin_app/app/external/repositories/local/local_combination_repository.dart';
+import 'package:coin_app/app/external/repositories/local/local_currency_repository.dart';
+import 'package:coin_app/app/interactor/contracts/repositories/combination_repository.dart';
+import 'package:coin_app/app/interactor/contracts/repositories/currencies_repository.dart';
+import 'package:coin_app/app/interactor/contracts/repositories/quote_repository.dart';
+import 'package:coin_app/app/interactor/contracts/repositories/variation_repository.dart';
+import 'package:coin_app/app/interactor/reducer/currency_reducer.dart';
+import 'package:flutter/material.dart';
+
+final autoInjector = AutoInjector();
+
+void initializeConfigurations() {
+  WidgetsFlutterBinding.ensureInitialized();
+}
+
+void registerInstances() {
+  // registering repositories
+  autoInjector.addSingleton<ICurrencyRepository>(LocalCurrencyRepository.new);
+  autoInjector.addSingleton<ICombinationRepository>(LocalCombinationRepository.new);
+  autoInjector.addSingleton<IQuoteRepository>(HttpQuoteRepository.new);
+  autoInjector.addSingleton<IVariationRepository>(HttpVariationRepository.new);
+  // registering reducers
+  autoInjector.addLazySingleton(CurrencyReducer.new);
+
+  autoInjector.commit();
+
+  // instance reducers
+  autoInjector.get<CurrencyReducer>();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:asp/asp.dart';
 import 'package:coin_app/app/app.dart';
+import 'package:coin_app/app/injector.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  initializeConfigurations();
+  registerInstances();
+
   runApp(const RxRoot(child: MyApp()));
 }
